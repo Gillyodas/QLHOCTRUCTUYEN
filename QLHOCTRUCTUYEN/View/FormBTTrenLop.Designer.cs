343018f8d893caba622b01446a53957b97f9ff4a
Gill <daylatkrac123@gmail.com>
2025-01-01 15:07:53 +0700

    Xong DN/DK

--- QLHOCTRUCTUYEN/View/FormBTTrenLop.Designer.cs
+++ QLHOCTRUCTUYEN/View/FormBTTrenLop.Designer.cs
@@ -29,29 +29,39 @@
         private void InitializeComponent()
         {
             this.btnUser = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTrangChu = new System.Windows.Forms.Button();
+            this.grbMenuNguoiDung = new System.Windows.Forms.GroupBox();
             this.tabMenuPhongHoc = new System.Windows.Forms.TabControl();
             this.tabBangTin = new System.Windows.Forms.TabPage();
             this.tabBaiTap = new System.Windows.Forms.TabPage();
-            this.tabMoiNguoi = new System.Windows.Forms.TabPage();
-            this.tabDiem = new System.Windows.Forms.TabPage();
+            this.btnXoaBT = new System.Windows.Forms.Button();
+            this.btnSuaBT = new System.Windows.Forms.Button();
+            this.btnThemBT = new System.Windows.Forms.Button();
             this.lsvDSBaiTap = new System.Windows.Forms.ListView();
             this.colBaiTap = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colDaNop = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colChuaNop = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colNgayDang = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.btnThemBT = new System.Windows.Forms.Button();
-            this.btnSuaBT = new System.Windows.Forms.Button();
-            this.btnXoaBT = new System.Windows.Forms.Button();
             this.colThoiHan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabMoiNguoi = new System.Windows.Forms.TabPage();
+            this.tabDiem = new System.Windows.Forms.TabPage();
             this.listView1 = new System.Windows.Forms.ListView();
             this.colSinhVien = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colDiem1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1.SuspendLayout();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grbDaThamGia = new System.Windows.Forms.GroupBox();
+            this.btnPhongHocDaThamGia = new System.Windows.Forms.Button();
+            this.btnViecCanLam = new System.Windows.Forms.Button();
+            this.grbGiangDay = new System.Windows.Forms.GroupBox();
+            this.btnPhongHocDaTao = new System.Windows.Forms.Button();
+            this.btnCanXemXet = new System.Windows.Forms.Button();
+            this.grbMenuNguoiDung.SuspendLayout();
             this.tabMenuPhongHoc.SuspendLayout();
             this.tabBaiTap.SuspendLayout();
             this.tabDiem.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.grbDaThamGia.SuspendLayout();
+            this.grbGiangDay.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnUser
@@ -63,25 +73,25 @@
             this.btnUser.Text = "User";
             this.btnUser.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnTrangChu
             // 
-            this.button1.Location = new System.Drawing.Point(915, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 42);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTrangChu.Location = new System.Drawing.Point(6, 11);
+            this.btnTrangChu.Name = "btnTrangChu";
+            this.btnTrangChu.Size = new System.Drawing.Size(65, 42);
+            this.btnTrangChu.TabIndex = 1;
+            this.btnTrangChu.Text = "Trang chủ";
+            this.btnTrangChu.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // grbMenuNguoiDung
             // 
-            this.groupBox1.Controls.Add(this.btnUser);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(1, 1);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1048, 71);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = " ";
+            this.grbMenuNguoiDung.Controls.Add(this.btnUser);
+            this.grbMenuNguoiDung.Controls.Add(this.btnTrangChu);
+            this.grbMenuNguoiDung.Location = new System.Drawing.Point(1, 1);
+            this.grbMenuNguoiDung.Name = "grbMenuNguoiDung";
+            this.grbMenuNguoiDung.Size = new System.Drawing.Size(1048, 71);
+            this.grbMenuNguoiDung.TabIndex = 2;
+            this.grbMenuNguoiDung.TabStop = false;
+            this.grbMenuNguoiDung.Text = " ";
             // 
             // tabMenuPhongHoc
             // 
@@ -89,10 +99,10 @@
             this.tabMenuPhongHoc.Controls.Add(this.tabBaiTap);
             this.tabMenuPhongHoc.Controls.Add(this.tabMoiNguoi);
             this.tabMenuPhongHoc.Controls.Add(this.tabDiem);
-            this.tabMenuPhongHoc.Location = new System.Drawing.Point(308, 78);
+            this.tabMenuPhongHoc.Location = new System.Drawing.Point(201, 78);
             this.tabMenuPhongHoc.Name = "tabMenuPhongHoc";
             this.tabMenuPhongHoc.SelectedIndex = 0;
-            this.tabMenuPhongHoc.Size = new System.Drawing.Size(741, 463);
+            this.tabMenuPhongHoc.Size = new System.Drawing.Size(848, 478);
             this.tabMenuPhongHoc.TabIndex = 0;
             // 
             // tabBangTin
@@ -100,7 +110,7 @@
             this.tabBangTin.Location = new System.Drawing.Point(4, 25);
             this.tabBangTin.Name = "tabBangTin";
             this.tabBangTin.Padding = new System.Windows.Forms.Padding(3);
-            this.tabBangTin.Size = new System.Drawing.Size(733, 434);
+            this.tabBangTin.Size = new System.Drawing.Size(840, 449);
             this.tabBangTin.TabIndex = 0;
             this.tabBangTin.Text = "Bảng tin";
             this.tabBangTin.UseVisualStyleBackColor = true;
@@ -119,25 +129,32 @@
             this.tabBaiTap.Text = "Bài tập trên lớp";
             this.tabBaiTap.UseVisualStyleBackColor = true;
             // 
-            // tabMoiNguoi
+            // btnXoaBT
             // 
-            this.tabMoiNguoi.Location = new System.Drawing.Point(4, 25);
-            this.tabMoiNguoi.Name = "tabMoiNguoi";
-            this.tabMoiNguoi.Padding = new System.Windows.Forms.Padding(3);
-            this.tabMoiNguoi.Size = new System.Drawing.Size(733, 434);
-            this.tabMoiNguoi.TabIndex = 2;
-            this.tabMoiNguoi.Text = "Mọi người";
-            this.tabMoiNguoi.UseVisualStyleBackColor = true;
+            this.btnXoaBT.Location = new System.Drawing.Point(652, 6);
+            this.btnXoaBT.Name = "btnXoaBT";
+            this.btnXoaBT.Size = new System.Drawing.Size(75, 51);
+            this.btnXoaBT.TabIndex = 3;
+            this.btnXoaBT.Text = "Xóa";
+            this.btnXoaBT.UseVisualStyleBackColor = true;
             // 
-            // tabDiem
+            // btnSuaBT
             // 
-            this.tabDiem.Controls.Add(this.listView1);
-            this.tabDiem.Location = new System.Drawing.Point(4, 25);
-            this.tabDiem.Name = "tabDiem";
-            this.tabDiem.Size = new System.Drawing.Size(733, 434);
-            this.tabDiem.TabIndex = 3;
-            this.tabDiem.Text = "Điểm";
-            this.tabDiem.UseVisualStyleBackColor = true;
+            this.btnSuaBT.Location = new System.Drawing.Point(571, 6);
+            this.btnSuaBT.Name = "btnSuaBT";
+            this.btnSuaBT.Size = new System.Drawing.Size(75, 51);
+            this.btnSuaBT.TabIndex = 2;
+            this.btnSuaBT.Text = "Sửa";
+            this.btnSuaBT.UseVisualStyleBackColor = true;
+            // 
+            // btnThemBT
+            // 
+            this.btnThemBT.Location = new System.Drawing.Point(490, 6);
+            this.btnThemBT.Name = "btnThemBT";
+            this.btnThemBT.Size = new System.Drawing.Size(75, 51);
+            this.btnThemBT.TabIndex = 1;
+            this.btnThemBT.Text = "Thêm";
+            this.btnThemBT.UseVisualStyleBackColor = true;
             // 
             // lsvDSBaiTap
             // 
@@ -175,37 +192,30 @@
             this.colNgayDang.Text = "Ngày đăng";
             this.colNgayDang.Width = 120;
             // 
-            // btnThemBT
-            // 
-            this.btnThemBT.Location = new System.Drawing.Point(490, 6);
-            this.btnThemBT.Name = "btnThemBT";
-            this.btnThemBT.Size = new System.Drawing.Size(75, 51);
-            this.btnThemBT.TabIndex = 1;
-            this.btnThemBT.Text = "Thêm";
-            this.btnThemBT.UseVisualStyleBackColor = true;
-            // 
-            // btnSuaBT
-            // 
-            this.btnSuaBT.Location = new System.Drawing.Point(571, 6);
-            this.btnSuaBT.Name = "btnSuaBT";
-            this.btnSuaBT.Size = new System.Drawing.Size(75, 51);
-            this.btnSuaBT.TabIndex = 2;
-            this.btnSuaBT.Text = "Sửa";
-            this.btnSuaBT.UseVisualStyleBackColor = true;
-            // 
-            // btnXoaBT
-            // 
-            this.btnXoaBT.Location = new System.Drawing.Point(652, 6);
-            this.btnXoaBT.Name = "btnXoaBT";
-            this.btnXoaBT.Size = new System.Drawing.Size(75, 51);
-            this.btnXoaBT.TabIndex = 3;
-            this.btnXoaBT.Text = "Xóa";
-            this.btnXoaBT.UseVisualStyleBackColor = true;
-            // 
             // colThoiHan
             // 
             this.colThoiHan.Text = "Thời hạn";
             this.colThoiHan.Width = 120;
+            // 
+            // tabMoiNguoi
+            // 
+            this.tabMoiNguoi.Location = new System.Drawing.Point(4, 25);
+            this.tabMoiNguoi.Name = "tabMoiNguoi";
+            this.tabMoiNguoi.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMoiNguoi.Size = new System.Drawing.Size(733, 434);
+            this.tabMoiNguoi.TabIndex = 2;
+            this.tabMoiNguoi.Text = "Mọi người";
+            this.tabMoiNguoi.UseVisualStyleBackColor = true;
+            // 
+            // tabDiem
+            // 
+            this.tabDiem.Controls.Add(this.listView1);
+            this.tabDiem.Location = new System.Drawing.Point(4, 25);
+            this.tabDiem.Name = "tabDiem";
+            this.tabDiem.Size = new System.Drawing.Size(733, 434);
+            this.tabDiem.TabIndex = 3;
+            this.tabDiem.Text = "Điểm";
+            this.tabDiem.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
@@ -225,19 +235,91 @@
             this.colDiem1.Text = "Điểm";
             this.colDiem1.Width = 57;
             // 
-            // Form1
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grbDaThamGia);
+            this.groupBox2.Controls.Add(this.grbGiangDay);
+            this.groupBox2.Location = new System.Drawing.Point(1, 63);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 493);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // grbDaThamGia
+            // 
+            this.grbDaThamGia.Controls.Add(this.btnPhongHocDaThamGia);
+            this.grbDaThamGia.Controls.Add(this.btnViecCanLam);
+            this.grbDaThamGia.Location = new System.Drawing.Point(0, 147);
+            this.grbDaThamGia.Name = "grbDaThamGia";
+            this.grbDaThamGia.Size = new System.Drawing.Size(200, 126);
+            this.grbDaThamGia.TabIndex = 2;
+            this.grbDaThamGia.TabStop = false;
+            this.grbDaThamGia.Text = "Đã tham gia";
+            // 
+            // btnPhongHocDaThamGia
+            // 
+            this.btnPhongHocDaThamGia.Location = new System.Drawing.Point(3, 67);
+            this.btnPhongHocDaThamGia.Name = "btnPhongHocDaThamGia";
+            this.btnPhongHocDaThamGia.Size = new System.Drawing.Size(195, 40);
+            this.btnPhongHocDaThamGia.TabIndex = 2;
+            this.btnPhongHocDaThamGia.Text = "Lớp 2";
+            this.btnPhongHocDaThamGia.UseVisualStyleBackColor = true;
+            // 
+            // btnViecCanLam
+            // 
+            this.btnViecCanLam.Location = new System.Drawing.Point(3, 21);
+            this.btnViecCanLam.Name = "btnViecCanLam";
+            this.btnViecCanLam.Size = new System.Drawing.Size(195, 40);
+            this.btnViecCanLam.TabIndex = 1;
+            this.btnViecCanLam.Text = "Việc cần làm";
+            this.btnViecCanLam.UseVisualStyleBackColor = true;
+            // 
+            // grbGiangDay
+            // 
+            this.grbGiangDay.Controls.Add(this.btnPhongHocDaTao);
+            this.grbGiangDay.Controls.Add(this.btnCanXemXet);
+            this.grbGiangDay.Location = new System.Drawing.Point(0, 15);
+            this.grbGiangDay.Name = "grbGiangDay";
+            this.grbGiangDay.Size = new System.Drawing.Size(200, 126);
+            this.grbGiangDay.TabIndex = 1;
+            this.grbGiangDay.TabStop = false;
+            this.grbGiangDay.Text = "Giảng dạy";
+            // 
+            // btnPhongHocDaTao
+            // 
+            this.btnPhongHocDaTao.Location = new System.Drawing.Point(3, 67);
+            this.btnPhongHocDaTao.Name = "btnPhongHocDaTao";
+            this.btnPhongHocDaTao.Size = new System.Drawing.Size(195, 40);
+            this.btnPhongHocDaTao.TabIndex = 2;
+            this.btnPhongHocDaTao.Text = "Lớp 1";
+            this.btnPhongHocDaTao.UseVisualStyleBackColor = true;
+            // 
+            // btnCanXemXet
+            // 
+            this.btnCanXemXet.Location = new System.Drawing.Point(3, 21);
+            this.btnCanXemXet.Name = "btnCanXemXet";
+            this.btnCanXemXet.Size = new System.Drawing.Size(195, 40);
+            this.btnCanXemXet.TabIndex = 1;
+            this.btnCanXemXet.Text = "Cần xem xét";
+            this.btnCanXemXet.UseVisualStyleBackColor = true;
+            // 
+            // FormBTTrenLop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1050, 553);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.tabMenuPhongHoc);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.grbMenuNguoiDung);
+            this.Name = "FormBTTrenLop";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
+            this.grbMenuNguoiDung.ResumeLayout(false);
             this.tabMenuPhongHoc.ResumeLayout(false);
             this.tabBaiTap.ResumeLayout(false);
             this.tabDiem.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.grbDaThamGia.ResumeLayout(false);
+            this.grbGiangDay.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -245,8 +327,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnUser;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnTrangChu;
+        private System.Windows.Forms.GroupBox grbMenuNguoiDung;
         private System.Windows.Forms.TabControl tabMenuPhongHoc;
         private System.Windows.Forms.TabPage tabBangTin;
         private System.Windows.Forms.TabPage tabBaiTap;
@@ -264,6 +346,13 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader colSinhVien;
         private System.Windows.Forms.ColumnHeader colDiem1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox grbDaThamGia;
+        private System.Windows.Forms.Button btnPhongHocDaThamGia;
+        private System.Windows.Forms.Button btnViecCanLam;
+        private System.Windows.Forms.GroupBox grbGiangDay;
+        private System.Windows.Forms.Button btnPhongHocDaTao;
+        private System.Windows.Forms.Button btnCanXemXet;
     }
 }
 
--- QLHOCTRUCTUYEN/View/FormDangKy.cs
+++ QLHOCTRUCTUYEN/View/FormDangKy.cs
@@ -18,6 +18,7 @@ namespace QLHOCTRUCTUYEN
     {
         private string connStr = ConfigurationManager.ConnectionStrings["QLHOCTRUCTUYEN"].ConnectionString;
         private bool isValidHoTen = false, isValidEmail = false, isValidPass = false, isValidReEnterPass = false;
+        private bool isValidEmailDN = false, isValidPassDN = false;
         //public event SentDataSighUpUser sentdata;
         //private ControlUsers ControlUsers;
         public FormDangKy()
@@ -25,27 +26,57 @@ namespace QLHOCTRUCTUYEN
             InitializeComponent();
             //ControlUsers = new ControlUsers(this);
             UpdateBtnDangKyState();
+            UpdateBtnDangNhapState();
         }
-
         private void UpdateBtnDangKyState()
         {
             btnDangKy.Enabled = isValidHoTen && isValidEmail && isValidPass && isValidReEnterPass;
         }
-
-
-        public static bool IsValidEmail(string email)
+        private void UpdateBtnDangNhapState()
+        {
+            btnDangNhapDN.Enabled = isValidEmailDN && isValidPassDN;
+        }
+        public static bool FuncIsValidEmail(string email)
         {
             string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]{2,6}$";
             return Regex.IsMatch(email, pattern);
         }
-
         private void txtHoTenIsValid(object sender, EventArgs e)
         {
             isValidHoTen = !string.IsNullOrWhiteSpace(txtHoTen.Text);
             lblBatBuocHoTen.Text = isValidHoTen ? "" : "Nhập họ tên";
             UpdateBtnDangKyState();
         }
-
+        private void txtEmailIsValid(object sender, EventArgs e)
+        {
+            isValidEmail = !string.IsNullOrWhiteSpace(txtEmail.Text) && FuncIsValidEmail(txtEmail.Text);
+            lblUnvalidEmail.Text = isValidEmail ? "" : "example@gmail.com";
+            UpdateBtnDangKyState();
+        }
+        private void txtPassIsValid(object sender, EventArgs e)
+        {
+            isValidPass = !string.IsNullOrWhiteSpace(txtPass.Text);
+            lblUnvalidPass.Text = isValidPass ? "" : "Nhập mật khẩu";
+            UpdateBtnDangKyState();
+        }
+        private void txtReEnterPassIsValid(object sender, EventArgs e)
+        {
+            isValidReEnterPass = txtReEnterPass.Text == txtPass.Text && !string.IsNullOrWhiteSpace(txtReEnterPass.Text);
+            lblUnvalidReEnterPass.Text = isValidReEnterPass ? "" : "Nhập lại phải giống mật khẩu";
+            UpdateBtnDangKyState();
+        }
+        private void txtEmailDN_TextChanged(object sender, EventArgs e)
+        {
+            isValidEmailDN = !string.IsNullOrWhiteSpace(txtEmailDN.Text) && FuncIsValidEmail(txtEmailDN.Text);
+            lblUnvalidEmailDN.Text = isValidEmailDN ? "" : "example@gmail.com";
+            UpdateBtnDangNhapState();
+        }
+        private void txtPassDN_TextChanged(object sender, EventArgs e)
+        {
+            isValidPassDN = !string.IsNullOrWhiteSpace(txtPassDN.Text);
+            lblUnvalidPassDN.Text = isValidPassDN ? "" : "Nhập mật khẩu";
+            UpdateBtnDangNhapState();
+        }
         private void btnDangKy_Click(object sender, EventArgs e)
         {
             Control.ControlUsers.ControlCreateUser(txtHoTen.Text, txtEmail.Text, txtPass.Text);
@@ -59,7 +90,10 @@ namespace QLHOCTRUCTUYEN
             //else MessageBox.Show("Loi");
 
         }
-
+        private void btnDangNhapDN_Click(object sender, EventArgs e)
+        {
+            Control.ControlUsers.ControlLogin(txtEmailDN.Text, txtPassDN.Text);
+        }
         public void DangKyThanhCong(bool success)
         {
             if (success)
@@ -68,39 +102,33 @@ namespace QLHOCTRUCTUYEN
             }
             else MessageBox.Show("That bai");
         }
-
-        private void btnDangNhap_Click(object sender, EventArgs e)
+        public void DangNhapThanhcong(bool success)
         {
-            FormDangNhap formDN = new FormDangNhap();
-            formDN.Show();
-            this.Hide();
-        }
+            if (success)
+            {
 
+                FormBTTrenLop formBTTrenLop = new FormBTTrenLop();
+                formBTTrenLop.Show();
+
+            }
+            else MessageBox.Show("Dang nhap that bai");
+            this.Hide(); // Đóng Form 2 từ Form 3.
+        }
+        private void btnChuyenSangFormDK_Click(object sender, EventArgs e)
+        {
+            panDangNhap.Visible = false;
+            panDangKy.Visible = true;
+        }
+        private void btnChuyenSangFormDN_Click(object sender, EventArgs e)
+        {
+            panDangKy.Visible= false;
+            panDangNhap.Visible = true;
+        }
         private void lblQuenMK_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void txtEmailIsValid(object sender, EventArgs e)
-        {
-            isValidEmail = !string.IsNullOrWhiteSpace(txtEmail.Text) && IsValidEmail(txtEmail.Text);
-            lblUnvalidEmail.Text = isValidEmail ? "" : "example@gmail.com";
-            UpdateBtnDangKyState();
-        }
-
-        private void txtPassIsValid(object sender, EventArgs e)
-        {
-            isValidPass = !string.IsNullOrWhiteSpace(txtPass.Text);
-            lblUnvalidPass.Text = isValidPass ? "" : "Nhập mật khẩu";
-            UpdateBtnDangKyState();
-        }
-
-        private void txtReEnterPassIsValid(object sender, EventArgs e)
-        {
-            isValidReEnterPass = txtReEnterPass.Text == txtPass.Text && !string.IsNullOrWhiteSpace(txtReEnterPass.Text);
-            lblUnvalidReEnterPass.Text = isValidReEnterPass ? "" : "Nhập lại phải giống mật khẩu";
-            UpdateBtnDangKyState();
-        }
     }
 
     public class DataSentSighUp : EventArgs
